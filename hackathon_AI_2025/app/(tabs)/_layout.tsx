fdfe74ecd2f5eb84d3d817fdd4a196e5177c6815
jlam303 <jplam904@gmail.com>
2025-11-10 04:40:06 -0700

    UI changes

--- hackathon_AI_2025/app/(tabs)/_layout.tsx
+++ hackathon_AI_2025/app/(tabs)/_layout.tsx
@@ -1,14 +1,15 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { Platform, StyleSheet, View,Text } from 'react-native';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import { AntDesign, FontAwesome5, MaterialIcons, Ionicons } from '@expo/vector-icons';
+import { FontAwesome6, MaterialIcons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
+  const iconSize = 24;
   return (
     // <Drawer
     //   screenOptions={{ drawerActiveTintColor: 'blue', headerShown: false }}>
@@ -23,52 +24,80 @@ export default function TabLayout() {
     // </Drawer>
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "#1C6E8C",
+        tabBarInactiveTintColor:"#a6a6a6",
         headerShown: false,
         tabBarButton: HapticTab,
+        tabBarStyle:{
+          backgroundColor:"white",
+          outline:"none",
+          borderColor:"#d9d9d9",
+          borderTopWidth:2,
+          height:80,
+          justifyContent:"center",
+        },
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: () => <AntDesign name="home" size={16} color="white" />,
+          tabBarLabel: () => null,
+          tabBarIcon: () => 
+            <View style={styles.buttonContainer}>
+              <FontAwesome6 name="house-chimney" size={iconSize} style={styles.icons} />
+              <Text style={styles.iconText}>Home</Text>
+            </View>
         }}
       />
       <Tabs.Screen
         name="memory"
         options={{
           title: 'Memory',
-          tabBarIcon: () => <FontAwesome5 name="brain" size={16} color="white" />,
+          tabBarIcon: () => <FontAwesome6 name="brain" size={iconSize} style={styles.icons} />,
         }}
       />
       <Tabs.Screen
         name="planning"
         options={{
           title: 'Planning',
-          tabBarIcon: () => <FontAwesome5 name="calendar-alt" size={16} color="white" />,
+          tabBarIcon: () => <FontAwesome6 name="calendar-check" size={iconSize} style={styles.icons} />,
         }}
       />
       <Tabs.Screen
         name="audio"
         options={{
           title: 'Audio',
-          tabBarIcon: () => <MaterialIcons name="multitrack-audio" size={16} color="white" />,
+          tabBarIcon: () => <MaterialIcons name="multitrack-audio" size={iconSize} style={styles.icons} />,
         }}
       />
       <Tabs.Screen
         name="self-control"
         options={{
           title: 'Self-Control',
-          tabBarIcon: () => <Ionicons name="happy-outline" size={16} color="white" />,
+          tabBarIcon: () => <FontAwesome6 name="face-laugh-beam" size={iconSize} style={styles.icons} />,
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: 'Settings',
-          tabBarIcon: () => <AntDesign name="setting" size={16} color="white" />,
+          tabBarIcon: () => <FontAwesome6 name="gear" size={iconSize} style={styles.icons} />,
         }}
       />
     </Tabs>
   );
 }
+const styles = StyleSheet.create({
+  icons:{
+    color:"#1C6E8C",
+  },
+  iconText:{
+    color:"#1C6E8C",
+    fontSize:16,
+    fontWeight:"700",
+  },
+  buttonContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
